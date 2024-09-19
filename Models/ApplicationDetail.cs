f35317d90bdc9d66151c7e84c6a8496e99fc858f
Raja Rajeshwari <rajeshwari.v@kgisl.com>
2024-09-19 18:25:22 +0530

    All-Validation-Completed

--- Models/ApplicationDetail.cs
+++ Models/ApplicationDetail.cs
@@ -40,9 +40,9 @@ namespace DolphinFx.Models
     public virtual UserRole? UserRole { get; set; } // 
 
 
-     [Required]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])[A-Za-z]{8,}$",
-            ErrorMessage = "Password must be at least 8 characters long and include both uppercase and lowercase letters.")]
+   [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@!#$%^&*])[A-Za-z\d@!#$%^&*]{8}$",
+            ErrorMessage = "Password must be exactly 8 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
     public string Password { get; set; } = string.Empty;
 
     //[Required]
--- Models/DatabaseDetail.cs
+++ Models/DatabaseDetail.cs
@@ -15,9 +15,14 @@ namespace DolphinFx.Models
         //public string? ApplicationName { get; set; }
         [Required]
         public string? Datasource { get; set; }
-        [Required]
+       [Required(ErrorMessage = "User Name is required.")]
+    [RegularExpression(@"^[a-zA-Z][a-zA-Z0-9\s]*$", ErrorMessage = "UserName must start with a letter and contain only letters, numbers, and spaces.")]
+    [StringLength(20, ErrorMessage = "UserName should not contain more than 20 characters.")]
         public string? Username { get; set; }
         [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@!#$%^&*])[A-Za-z\d@!#$%^&*]{8}$",
+            ErrorMessage = "Password must be exactly 8 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+
         public string? Password { get; set; }
 
         [ForeignKey("Client")]
