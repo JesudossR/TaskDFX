659cb744db1e5d0f6077b2abbead411bb589501c
Raja Rajeshwari <rajeshwari.v@kgisl.com>
2024-09-27 15:34:35 +0530

    datatype_upadted

--- Models/Client.cs
+++ Models/Client.cs
@@ -22,17 +22,17 @@ namespace DolphinFx.Models
         [Required(ErrorMessage = "Primary Contact is required.")]
         [RegularExpression(@"^\d{10}$", ErrorMessage = "Primary Contact must be a 10-digit number.")]
         [Column("PRIMARY_CONTACT")]
-        public long PrimaryContact { get; set; } // 10 digits
+        public string? PrimaryContact { get; set; } = string.Empty;// 10 digits
 
         [Required(ErrorMessage = "Primary Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email format for Primary Email.")]
         [RegularExpression(@"^[\w\.-]+@[a-zA-Z\d\.-]+(\.com|\.co\.in|\.in)$", ErrorMessage = "Email must end with '.com', '.co.in', or '.in'")]
         [Column("PRIMARY_EMAILID")]
-        public string PrimaryEmailID { get; set; } = string.Empty; // Required and valid email
+        public string? PrimaryEmailID { get; set; } = string.Empty; // Required and valid email
 
         [RegularExpression(@"^\d{10}$", ErrorMessage = "Primary Contact must be a 10-digit number.")]
         [Column("SECONDARY_CONTACT")]
-        public long? SecondaryContact { get; set; } // 10 digits
+        public string? SecondaryContact { get; set; } = string.Empty;// 10 digits
 
         [EmailAddress(ErrorMessage = "Invalid email format for Secondary Email.")]
         [RegularExpression(@"^[\w\.-]+@[a-zA-Z\d\.-]+(\.com|\.co\.in|\.in)$", ErrorMessage = "Email must end with '.com', '.co.in', or '.in'")]
